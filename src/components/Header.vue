43f9643feee832c191ba89acf5500c628360adfb
evgeny <evgeny@evgeny-pc>
2020-02-15 21:15:22 +1100

    Updated Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    
+  <div class="header">
+    <b class="title">Brightness & Contrast Developer Test</b>
+    <p class="subtitle">01 Jun, 2018 - 31 Dec, 2019</p>
   </div>
 </template>
 
@@ -15,5 +16,21 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.header {
+    background-color: #7344C0;
+    padding-top: 1em;
+    padding-bottom: 1em;
+}
 
+.title, .subtitle {
+    color: white;
+}
+
+.title {
+  
+}
+
+.subtitle {
+  margin-top: 0.5em;
+}
 </style>
