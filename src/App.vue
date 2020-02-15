bafc987b9f8caa7b4f0c67dbfd0b8dde046c9adf
evgeny <evgeny@evgeny-pc>
2020-02-15 21:14:58 +1100

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <div id="app">
-    
+    <Main></Main>
   </div>
 </template>
 
 <script>
+import Main from './views/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    
+    Main
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  outline: none;
+}
+
+html, body {
+  width: 100%;
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
