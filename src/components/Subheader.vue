ff408d8da83adad8f2b6d14579f9987c03be9005
evgeny <evgeny@evgeny-pc>
2020-02-15 21:15:33 +1100

    Updated Subheader.vue

--- src/components/Subheader.vue
+++ src/components/Subheader.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    
+    <div class="subheader"></div>
+    <div class="avatar"></div>
   </div>
 </template>
 
@@ -15,5 +16,23 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.subheader {
+  height: 15em;
+  background-image: url("/img/header.png");
+  background-size: cover;
+  background-position: center;
+}
 
+.avatar {
+  height: 7em;
+  width: 7em;
+  background-image: url("/img/avatar.png");
+  background-size: cover;
+  background-position: center;
+  border-radius: 50%;
+  margin-top: -6em;
+  border: 0.3em solid #fff;
+  margin-left: auto;
+  margin-right: auto;
+}
 </style>
