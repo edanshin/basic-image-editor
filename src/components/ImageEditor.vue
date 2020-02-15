f992411fcbd620e846e7ff7c4026c392e98fd9bf
evgeny <evgeny@evgeny-pc>
2020-02-15 21:11:53 +1100

    renamed Image.vue to ImageEditor.vue

---
+++ src/components/ImageEditor.vue
@@ -0,0 +1,152 @@
+<template>
+  <div>
+    <Slider type="brightness" ref="brightness" color="#25A95B" title="Brightness" subtitle="Slide to adjust image brightness! 🔆" :isDisabled="isDisabled" @valueChanged="valueChanged($event)"></Slider>
+    <Slider type="contrast" ref="contrast" color="#4A90E2" title="Contrast" subtitle="Slide to adjust image contrast! 🌓" :isDisabled="isDisabled" @valueChanged="valueChanged($event)"></Slider>
+    <div class="container">
+      <canvas id="myCanvas" style="width: 100%;"></canvas>
+      <br>
+        <div style="display: flex; justify-content: center;">
+          <p class="label">NAME</p><p class="name">{{filename}}</p>
+
+          <div class="upload-btn-wrapper">
+            <button class="uploadBtn">▲ UPLOAD</button>
+            <input type="file" id="file_input" @change="upload"/>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Slider from './Slider.vue'
+import ARimage from '../utils/arimage'
+
+export default {
+  name: 'ImageEditor',
+  props: {
+    source: ""
+  },
+
+  data: function() {
+    return {
+      canvas: null,
+      context: null,
+      filename: "",
+      imageData: null,
+      sourceImage: null,
+      brightness: 0,
+      contrast: 0,
+      isDisabled: true
+    }
+  },
+
+  components: {
+    Slider
+  },
+
+  methods: {
+    upload(event) {
+      var URL = window.webkitURL || window.URL;
+      var u = URL.createObjectURL(event.target.files[0]);
+      var img = new Image();
+      img.src = u;
+      var t = this;
+
+      this.filename = event.target.files[0].name;
+
+      img.onload = function() {
+        t.$refs.brightness.value = 0;
+        t.$refs.contrast.value = 0;
+        t.context.clearRect(0, 0, t.canvas.width, t.canvas.height);
+        ARimage.LoadToCanvas(t.canvas, img, t.context);
+        t.isDisabled = false;
+      }
+
+      this.sourceImage = img;
+    },
+
+    valueChanged(event) {
+      ARimage.LoadToCanvas(this.canvas, this.sourceImage, this.context);
+      this.imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
+
+      switch (event.type) {
+        case "brightness":
+          this.brightness = event.value
+          break;
+        case "contrast":
+          this.contrast = event.value
+          break;
+      }
+
+      ARimage.ModifyBrightness(this.imageData.data, parseInt(this.brightness, 10));
+      ARimage.ModifyContrast(this.imageData.data, parseInt(this.contrast, 10));
+
+      this.context.putImageData(this.imageData, 0, 0);
+    }
+  },
+
+  mounted: function() {
+    this.canvas = document.getElementById("myCanvas");
+    this.context = this.canvas.getContext("2d");
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  border: 0.1em solid #DCDEE4;
+  border-radius: 0.5em;
+
+  margin: 1em;
+
+  padding-bottom: 0.5em;
+}
+
+.label {
+  border: 0.1em solid #DCDEE4;
+  border-top-left-radius: 0.5em;
+  border-bottom-left-radius: 0.5em;
+  padding: 0.5em;
+  background-color: #F6F8FA;
+  color: #8392A6;
+  font-weight: bold;
+}
+
+.name {
+  border: 0.1em solid #DCDEE4;
+  border-top-right-radius: 0.5em;
+  border-bottom-right-radius: 0.5em;
+  padding: 0.5em;
+  margin-left: -0.1em;
+  width: 8em;
+  max-width: 8em;
+  text-overflow: ellipsis;
+  overflow-x: hidden;
+  color: #25A95B;
+}
+
+.upload-btn-wrapper {
+  position: relative;
+  overflow: hidden;
+  display: inline;
+  margin-left: 1em;
+}
+
+.uploadBtn {
+  border: 0.1em solid #DCDEE4;
+  color: #6FA6E6;
+  background-color: #F6F8FA;
+  border-radius: 0.5em;
+  font-weight: bold;
+  height: 100%;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+}
+
+.upload-btn-wrapper input[type=file] {
+  position: absolute;
+  left: 0;
+  top: 0;
+  opacity: 0;
+}
+</style>
