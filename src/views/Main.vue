8ede22b2cece2cee151e4ddc9333798760884635
evgeny <evgeny@evgeny-pc>
2020-02-15 21:13:21 +1100

    added Main.vue view

---
+++ src/views/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Subheader></Subheader>
+    <ImageEditor></ImageEditor>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+import Subheader from '../components/Subheader.vue'
+import ImageEditor from '../components/ImageEditor.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    Header,
+    Subheader,
+    ImageEditor
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
