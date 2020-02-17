3a6f094f1b6ed7fe850fc636c9602dec9119cfa5
evgeny <evgeny@evgeny-pc>
2020-02-17 16:08:13 +1100

    added media queries for max width, changed body background color

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="main">
     <Header></Header>
     <Subheader></Subheader>
     <ImageEditor></ImageEditor>
@@ -22,5 +22,49 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  background-color: #262626;
+}
 
+#main {
+  background-color: #FFFFFF;
+}
+
+@media screen and (min-width: 1920px) {
+  #main {
+    width: 20%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (min-width: 1440px) and (max-width: 1919px) {
+  #main {
+    width: 40%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (min-width: 1024px) and (max-width: 1439px) {
+  #main {
+    width: 60%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (min-width: 600px) and (max-width: 1023px) {
+  #main {
+    width: 80%;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media screen and (max-width: 599px) {
+  #main {
+    width: 100%;
+  }
+}
 </style>
