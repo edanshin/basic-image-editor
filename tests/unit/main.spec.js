a6eb6be224a2d723577f443950ca42441b4ba6e2
evgeny <evgeny@evgeny-pc>
2020-03-01 15:05:10 +1100

    Updated main.spec.js

--- tests/unit/main.spec.js
+++ tests/unit/main.spec.js
@@ -2,21 +2,19 @@ import ARimage from '@/utils/arimage.js'
 
 describe('ARimage', () => {
   const imageData = [24,248,185,97,78,212,150,44,104,33,203,94,52,164,124,60];
-  const brightness = 50;
-  const contrast = -50;
 
   it('changes brightness and keeps contrast', () => {
     // apply contrast adjustment over brightness adjustment
-    ARimage.ModifyBrightness(imageData, parseInt(brightness, 10));
-    ARimage.ModifyContrast(imageData, parseInt(contrast, 10));
+    ARimage.ModifyBrightness(imageData, parseInt(35, 10));
+    ARimage.ModifyContrast(imageData, parseInt(-35, 10));
 
     expect(imageData).toMatch([])
   }),
 
   it('changes contrast and keeps brightness', () => {
     // apply contrast adjustment over brightness adjustment
-    ARimage.ModifyBrightness(imageData, parseInt(brightness, 10));
-    ARimage.ModifyContrast(imageData, parseInt(contrast, 10));
+    ARimage.ModifyBrightness(imageData, parseInt(-35, 10));
+    ARimage.ModifyContrast(imageData, parseInt(35, 10));
 
     expect(imageData).toMatch([])
   })
